08cd49fb940d3828266b00c6c4fabe214f996a0b
mahmoud nayel <mahmmoudnayel2004@gmail.com>
2025-04-14 17:17:06 +0200

    Footer Content

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
 		<footer
 			className={`${classes.Footer} w-full py-4 text-center bg-slate-800 text-white fixed bottom-0 left-0`}>
 			<div className='container mx-auto px-3'>
-				<h2>GBOY MADE A MASTERPIECE</h2>
+				<h2>FreshCart@2025</h2>
 			</div>
 		</footer>
 	);
